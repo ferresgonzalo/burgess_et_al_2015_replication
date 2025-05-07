e4cb6620d11916ff40a23288c24cbc51b1698c9c
Gonzalo Ferrés <79223231+ferresgonzalo@users.noreply.github.com>
2025-05-07 10:45:08 -0700

    add luminosity data per district

---
+++ extensions/append_luminosity.do
@@ -0,0 +1,28 @@
+set more off
+
+cd "."
+
+use luminosity/new_bltcfix_light_loc1992.dta, replace
+forvalues var = 1993/2017 {
+	append using luminosity/new_bltcfix_light_loc`var'.dta
+}
+
+g new_bltcfix_lit=1 if new_bltcfix_light>0
+replace new_bltcfix_lit=0 if new_bltcfix_light==0
+
+* define 1/2 minimum value for windsoring
+quietly sum new_bltcfix_light if new_bltcfix_light >0,d
+local min_value = r(min)/2
+di `min_value'
+
+g new_bltcfix_lnlight_c=ln(`min_value'+new_bltcfix_light)
+
+* prepare merge 
+rename ADMIN_NAME distname_1979
+replace distname_1979 = "Elgeyo Marakwet" if distname_1979 == "Elgeyo Karakwet"
+replace distname_1979 = "Machakos" if distname_1979 == "Mackakos"
+drop if distname_1979 == "Lake Turkana"
+drop if distname_1979 == "Lake Victoria"
+drop if year > 2011
+
+save luminosity/newlight_bltcfix_loc.dta, replace
---
+++ extensions/get_luminosity.py
@@ -0,0 +1,54 @@
+######## Create zonal statistics of lights ########
+
+
+##### Previous steps
+
+# Importing libraries
+import geopandas as gpd
+import os
+import rasterio
+from pyproj import CRS
+from rasterstats import zonal_stats
+
+
+# Set working directory
+os.chdir('/Users/gonzaloferres/GitHub/burgess_et_al_2015_replication/extensions/')
+    
+# Define CRS
+wgs84_crs = CRS.from_string('EPSG:4326') # WGS 1984
+
+##### Create zonal statistics
+lights = {}
+years = list(range(1992, 2017 + 1))
+for year in years:
+    
+    # Import lights data
+    lights[year] = rasterio.open('./luminosity/DMSP' + str(year) + '_bltcfix.tif')
+    crs_raster = lights[year].crs
+    
+    # Import Kenyan 41 districts shapefile 
+    kenya = gpd.read_file('geo2_ke1969/geo2_ke1969.shp')[['ADMIN_NAME', 'geometry']].set_crs(wgs84_crs).to_crs(crs_raster)
+    
+    
+    array = lights[year].read(1)
+    affine = lights[year].transform
+
+    zs = zonal_stats(kenya, array, affine = affine, stats = ['mean', "sum"], nodata = -999, all_touched = True)
+    kenya['new_bltcfix_light'] = [x['mean'] for x in zs]
+    kenya['new_bltcfix_light_sum'] = [x['sum'] for x in zs]
+    
+    
+    binary_array = array.copy()
+    binary_array[binary_array > 0] = 1
+    binary_array[binary_array == 0] = 0
+
+    zs = zonal_stats(kenya, binary_array, affine = affine, stats=['mean', 'sum'], nodata = -999, all_touched = True)    
+    kenya['new_bltcfix_pixsh'] = [x['mean'] for x in zs]
+    kenya['new_bltcfix_sum'] = [x['sum'] for x in zs]
+    
+    
+    # export to Stata
+    del kenya['geometry']
+    kenya['year'] = year
+    kenya.to_stata('./luminosity/new_bltcfix_light_loc' + str(year) + '.dta', write_index = False)
+
